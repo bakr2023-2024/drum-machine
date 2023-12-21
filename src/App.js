dd1ec72b5c46df82c603d2f44c96cec3d60342fa
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2023-12-22 00:35:30 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+class DrumSet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.play = this.play.bind(this);
+  }
+  play(event) {
+    let div =
+      event.type === "click"
+        ? document.getElementById(event.target.textContent)
+        : document.getElementById(event.key.toUpperCase());
+    if (div) {
+      if (this.props.power) {
+        div.volume = this.props.volume / 100;
+        div.currentTime = null;
+        div.play();
+        this.props.changeDisplay(div.parentElement.id);
+      }
+      div.parentElement.animate(
+        [{}, { boxShadow: "none", backgroundColor: "orange" }],
+        {
+          duration: 150,
+          iterations: 1,
+        }
+      );
+    }
+  }
+  componentDidMount() {
+    window.addEventListener("keydown", this.play);
+  }
+  render() {
+    return (
+      <div
+        className="col-6 d-flex flex-wrap gap-2 justify-content-center align-items-stretch p-2"
+        id="drums"
+      >
+        {[...Array(9).keys()].map((i) => (
+          <div
+            id={this.props.set[i].id}
+            className="drum-pad"
+            key={i}
+            onClick={this.play}
+          >
+            <audio
+              src={this.props.set[i].url}
+              className="clip"
+              id={this.props.set[i].keyTrigger}
+            ></audio>
+            <p>{this.props.set[i].keyTrigger}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-
-export default App;
+class Controls extends React.Component {
+  render() {
+    return (
+      <div className="col-6 text-center" id="controls">
+        <div>
+          <h4>Power</h4>
+          <button
+            id="togglePowerBtn"
+            className={this.props.power ? "on" : "off"}
+            onClick={this.props.togglePower}
+          >
+            {this.props.power ? "ON" : "OFF"}
+          </button>
+        </div>
+        <div>
+          <h4 id="display">{this.props.display}</h4>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={this.props.volume}
+            className="form-range"
+            id="myRange"
+            onChange={this.props.changeVolume}
+          />
+        </div>
+        <div>
+          <h4>Bank</h4>
+          <button
+            id="toggleBankBtn"
+            className={this.props.bank ? "on" : "off"}
+            onClick={this.props.toggleBank}
+          >
+            {this.props.bank ? "ON" : "OFF"}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      power: true,
+      volume: "50",
+      bank: false,
+      display: "Heater Kit",
+    };
+    this.togglePower = this.togglePower.bind(this);
+    this.changeVolume = this.changeVolume.bind(this);
+    this.toggleBank = this.toggleBank.bind(this);
+    this.changeDisplay = this.changeDisplay.bind(this);
+  }
+  togglePower() {
+    this.setState((state) => ({
+      power: !state.power,
+    }));
+  }
+  changeVolume(event) {
+    if (this.state.power)
+      this.setState(() => ({
+        volume: event.target.value,
+        display: "Volume: " + event.target.value + "%",
+      }));
+  }
+  toggleBank() {
+    if (this.state.power)
+      this.setState((state) => ({
+        display: !state.bank ? "Smoooth Piano Kit" : "Heater Kit",
+        bank: !state.bank,
+      }));
+  }
+  changeDisplay(text) {
+    this.setState(() => ({
+      display: text,
+    }));
+  }
+  render() {
+    return (
+      <div className="row" id="drum-machine">
+        <DrumSet
+          power={this.state.power}
+          volume={this.state.volume}
+          set={this.state.bank ? this.props.piano : this.props.drum}
+          changeDisplay={this.changeDisplay}
+        />
+        <Controls
+          togglePower={this.togglePower}
+          changeVolume={this.changeVolume}
+          toggleBank={this.toggleBank}
+          power={this.state.power}
+          volume={this.state.volume}
+          bank={this.state.bank}
+          display={this.state.display}
+        />
+      </div>
+    );
+  }
+}
+export default Container;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,116 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from "react-dom/client";
+import Container from "./App";
+const e = [
+    {
+      keyCode: 81,
+      keyTrigger: "Q",
+      id: "Heater-1",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyCode: 87,
+      keyTrigger: "W",
+      id: "Heater-2",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyCode: 69,
+      keyTrigger: "E",
+      id: "Heater-3",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyCode: 65,
+      keyTrigger: "A",
+      id: "Heater-4",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyCode: 83,
+      keyTrigger: "S",
+      id: "Clap",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyCode: 68,
+      keyTrigger: "D",
+      id: "Open-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyCode: 90,
+      keyTrigger: "Z",
+      id: "Kick-n'-Hat",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyCode: 88,
+      keyTrigger: "X",
+      id: "Kick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyCode: 67,
+      keyTrigger: "C",
+      id: "Closed-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ],
+  t = [
+    {
+      keyCode: 81,
+      keyTrigger: "Q",
+      id: "Chord-1",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+    },
+    {
+      keyCode: 87,
+      keyTrigger: "W",
+      id: "Chord-2",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+    },
+    {
+      keyCode: 69,
+      keyTrigger: "E",
+      id: "Chord-3",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+    },
+    {
+      keyCode: 65,
+      keyTrigger: "A",
+      id: "Shaker",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+    },
+    {
+      keyCode: 83,
+      keyTrigger: "S",
+      id: "Open-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+    },
+    {
+      keyCode: 68,
+      keyTrigger: "D",
+      id: "Closed-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+    },
+    {
+      keyCode: 90,
+      keyTrigger: "Z",
+      id: "Punchy-Kick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+    },
+    {
+      keyCode: 88,
+      keyTrigger: "X",
+      id: "Side-Stick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+    },
+    {
+      keyCode: 67,
+      keyTrigger: "C",
+      id: "Snare",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+    },
+  ];
+const root = ReactDOM.createRoot(document.getElementById("hookContainer"));
+root.render(<Container drum={e} piano={t} />);
